728068d71a308e443251dd1f331290ab4b22cb32
Angel Calderon <angelcalderon303@gmail.com>
2024-09-24 16:26:49 -0400

    fix image saver

--- lib/ui/widgets/molecules/download_apod.dart
+++ lib/ui/widgets/molecules/download_apod.dart
@@ -1,6 +1,12 @@
 // image_saver.dart
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image/image.dart' as img;
+import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
+import 'package:flutter_file_dialog/flutter_file_dialog.dart';
 
 void saveNetworkImage(String imageUrl, String title) async {
   Fluttertoast.showToast(
@@ -8,13 +14,37 @@ void saveNetworkImage(String imageUrl, String title) async {
       toastLength: Toast.LENGTH_SHORT,
       gravity: ToastGravity.CENTER,
       fontSize: 16.0);
-  var response = await Dio()
-      .get(imageUrl, options: Options(responseType: ResponseType.bytes));
-  // await ImageGallerySaver.saveImage(Uint8List.fromList(response.data),
-  //     name: title);
-  Fluttertoast.showToast(
-      msg: "Operación completada con éxito.",
-      toastLength: Toast.LENGTH_SHORT,
-      gravity: ToastGravity.CENTER,
-      fontSize: 16.0);
+
+  try {
+    // Download image
+    final http.Response response = await http.get(Uri.parse(imageUrl));
+
+    // Get temporary directory
+    final dir = await getTemporaryDirectory();
+
+    // Create an image name
+    var filename = '${dir.path}/${title.replaceAll(' ', '')}.png';
+
+    // Save to filesystem
+    final file = File(filename);
+    await file.writeAsBytes(response.bodyBytes);
+
+    // Ask the user to save it
+    final params = SaveFileDialogParams(sourceFilePath: file.path);
+    final finalPath = await FlutterFileDialog.saveFile(params: params);
+
+    if (finalPath != null) {
+      Fluttertoast.showToast(
+          msg: "Operación completada con éxito.",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          fontSize: 16.0);
+    }
+  } catch (e) {
+    Fluttertoast.showToast(
+        msg: "No pudimos instalar la imagen",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.CENTER,
+        fontSize: 16.0);
+  }
 }
