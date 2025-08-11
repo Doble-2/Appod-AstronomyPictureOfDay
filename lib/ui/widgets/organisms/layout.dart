72c827ed63078d922ec5780ef6c9296f4d93233c
Angel Calderon <angelcalderon303@gmail.com>
2025-08-11 00:58:25 -0400

    Refactor: ajustar el padding en el Layout y agregar Google Analytics en index.html

--- lib/ui/widgets/organisms/layout.dart
+++ lib/ui/widgets/organisms/layout.dart
@@ -40,7 +40,7 @@ class Layout extends StatelessWidget {
                 children: [
                   Positioned.fill(
                     child: SingleChildScrollView(
-                      padding: const EdgeInsets.fromLTRB(6 , 12, 6,12),
+                      padding: const EdgeInsets.fromLTRB(6 , 12, 24,12),
                       child: bodyContent,
                     ),
                   ),
